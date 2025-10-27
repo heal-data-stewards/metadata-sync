f1a523b2025242dfaa4dbf5fa936da5d34be4e60
Hina Shah <hinashah@renci.org>
2025-10-27 10:57:59 -0400

    Update Query Progress Tracker to have PI names and lenient project number search

--- mds_api_service/query_progress_tracker_table.py
+++ mds_api_service/query_progress_tracker_table.py
@@ -4,6 +4,7 @@ import mysql.connector
 from decimal import Decimal
 from datetime import datetime
 from dotenv import load_dotenv
+import pymysql
 
 class EnhancedEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -20,7 +21,7 @@ def parse_json_fields(data):
             try:
                 data[field] = json.loads(data[field].replace("'", "\""))  # Replace single quotes with double quotes
             except json.JSONDecodeError:
-                pass  # If it fails, keep it as a string
+                data[field] = {} # If it fails, keep it as a string
     return data
 
 load_dotenv()
@@ -35,42 +36,48 @@ table_name = os.getenv('TABLE_NAME')
 def lambda_handler(event, context):
     appl_id = proj_num = hdp_id = ''
     results = []
-
+    
     try:
         appl_id = event['queryStringParameters'].get('appl_id', '')
         proj_num = event['queryStringParameters'].get('proj_num', '')
         hdp_id = event['queryStringParameters'].get('hdp_id', '').upper()
+        pi_name = event['queryStringParameters'].get('pi_name', '').lower()
+        
+        if len(appl_id) == 0 and len(proj_num) == 0 and len(hdp_id) == 0 and len(pi_name) == 0:
+            raise Exception("Please provide at least one of the following: appl_id, proj_num, hdp_id, pi_name")
 
-        if appl_id.startswith('CTN'):
-            normalized_appl_id = appl_id.replace('-', '')  # Remove dashes if CTN prefix is present
-        else:
-            normalized_appl_id = appl_id
-
-        normalized_proj_num = proj_num.replace('-', '')  # Remove dashes from proj_num
-
+        normalized_appl_id = appl_id
+        normalized_proj_num = '%' + proj_num.replace('-', '') + '%' # Remove dashes from proj_num
+        pi_name_pattern = '%' + pi_name + '%'
+        
         conn = mysql.connector.connect(
             user=db_username, 
             password=db_password, 
             host=db_host, 
             database=db_database
         )
-
+        
         cursor = conn.cursor()
-
+        
         # Sample query - modify as needed
-        query = f"SELECT * FROM {table_name} WHERE REPLACE(appl_id, '-', '')=%s OR REPLACE(project_num, '-', '')=%s OR REPLACE(hdp_id, '-', '')=%s;"
-        cursor.execute(query, (normalized_appl_id, normalized_proj_num, hdp_id))
-
+        if len(pi_name) > 0:
+            query = f"SELECT * FROM {table_name} WHERE LOWER(investigators_name) LIKE %s;"
+            cursor.execute(query, (pi_name_pattern,))
+        else:
+            query = f"SELECT * FROM {table_name} WHERE REPLACE(appl_id, '-', '')=%s OR REPLACE(project_num, '-', '') LIKE %s OR REPLACE(hdp_id, '-', '')=%s;"
+            cursor.execute(query, (normalized_appl_id, normalized_proj_num, hdp_id))
         # Check if the cursor.description is not None
         if cursor.description is not None:
             columns = [column[0] for column in cursor.description]
             rows = cursor.fetchall()
+            print(rows)
             for row in rows:
                 result = dict(zip(columns, row))
                 result = parse_json_fields(result)
                 results.append(result)
         else:
             results = "No results returned or query did not execute successfully."
+            print("There were no results?")
 
         # Close the cursor and the connection
         cursor.close()
@@ -89,6 +96,3 @@ def lambda_handler(event, context):
         },
         'body': json.dumps(results, cls=EnhancedEncoder)
     }
-
-# test = lambda_handler(0, 0)
-# print(test)
